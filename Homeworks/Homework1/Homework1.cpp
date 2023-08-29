e47f308820338a23914bdf5f6441cfd51b340902
Sergei Anisimov <s.anisimov.it@gmail.com>
2023-08-29 12:54:07 +0200

    Homework 1

---
+++ Homeworks/Homework1/Homework1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+float GetSideSize(char sideName){
+    float size;
+    std::cout << "\n Enter side " << sideName <<" lenght :";
+    std::cin >> size;
+    return size;
+}
+
+float GetResult(float aSide, float bSide){
+    float result;
+    return  result = sqrt(aSide*aSide + bSide*bSide);
+}
+
+int main()
+{
+    float aSide;
+    float bSide;
+
+    aSide = GetSideSize('a');
+    bSide = GetSideSize('b');
+    
+    std::cout << "\n c side lenght = " << GetResult(aSide, bSide) << std::endl;
+
+    return 0;
+}
